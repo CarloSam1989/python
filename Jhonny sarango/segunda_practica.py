c7eea7851ba7d6870c2a6aef2cab8334cda0d2ee
sarangosanchezj@gmail.com <https://github.com/CarloSam1989/pythonsarangosanchezj@gmail.com>
2023-05-31 22:20:55 -0500

    CAMBIOS

--- Jhonny sarango/segunda_practica.py
+++ Jhonny sarango/segunda_practica.py
@@ -9,41 +9,64 @@ print("El factorial de {} es {}".format(valor,factos))
 
 
 from datetime import date, timedelta
+
 print(date.today())  """""
+def suma(*numeros):
+   return sum(numeros)
 
-def suma(*parametos):
-    for i in range(valor):
-            num = float(input("Introduce el número #{}: ".format(i + 1)))
-            sum.append(num)
-        
-    for x in sum:
-        suma  = suma + x
-    print(suma)
+def restar(*numeros):
+   resultado = numeros[0]
+   for num in numeros[1:]:
+     resultado -= num
+     return resultado
 
+def multiplicar(*numeros):
+    resultado = numeros[0]
+    for num in numeros[1:]:
+     resultado *= num
+     return resultado
 
+def dividir(*numeros):
+    resultado = numeros[0]
+    for num in numeros[1:]:
+     resultado /= num
+     return resultado
+continuar=True
+while continuar:
+    print("----------MENU----------")
+    print("1: sumar")
+    print("2: restar")
+    print("3: multiplicar")
+    print("4: dividir")
+    print("5: salir del sytema")
+    print("-------------------------")
+    opcion = input("Ingrese una opción: ")
+    print("-------------------------")
+    if opcion == "1":
+        cantidad = int(input("Ingrese la cantidad de números a sumar: "))
+        print("-------------------------")
+        numeros = [float(input("Ingrese un número {}: ".format(i+1))) for i in range(cantidad)]
+        print("El resultado es:", suma(*numeros))
+    if opcion == "2":
+        cantidad = int(input("Ingrese la cantidad de números a restar: "))
+        print("-------------------------")
+        numeros = [float(input("Ingrese un número {}: ".format(i+1))) for i in range(cantidad)]
+        print("El resultado es:", restar(*numeros))
+    if opcion == "3":
+        cantidad = int(input("Ingrese la cantidad de números a multiplicar: "))
+        print("-------------------------")
+        numeros = [float(input("Ingrese un número {}: ".format(i+1))) for i in range(cantidad)]
+        print("El resultado es:", multiplicar(*numeros))
+    if opcion == "4":
+        cantidad = int(input("Ingrese la cantidad de números a dividir: "))
+        print("-------------------------")
+        numeros = [float(input("Ingrese un número {}: ".format(i+1))) for i in range(cantidad)]
+        print("El resultado es:", dividir(*numeros))        
+    if opcion == "5":
+       print("salir del systema")
+       continuar=False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""""
 continuar=True
 while continuar:
     valor= int (input("Ingrese la cantidad de numeros que desea operar : "))
@@ -114,7 +137,7 @@ while continuar:
         print("salindo del systema")
         print("-----------------------")    
     else:
-        print("no es una opcion valida")
+        print("no es una opcion valida")"""
 
 
 
