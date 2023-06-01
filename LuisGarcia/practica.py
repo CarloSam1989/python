d67633a9a63c3cf986945c0d8f703ff743779f0f
LuisGarcia202 <luisarmandogarcia141@gmail.com>
2023-05-31 19:59:08 -0500

    a

--- LuisGarcia/practica.py
+++ LuisGarcia/practica.py
@@ -3,4 +3,12 @@ valor= int(input("Ingrese un valor para calcular el factorial: "))
 factos= math.factorial(valor)
 print( "el factorial de {} es: {}".format(valor, factos))
 
+#calcular la fecha de nacimiento con la actual
+import datetime
+fecha_actual = datetime.date.today()
+fecha_nacimiento = input("Ingrese la fecha de nacimiento: ")
+fecha_nacimiento = datetime.datetime.strptime(fecha_nacimiento, "%d/%m/%Y")
+fecha_nacimiento = fecha_nacimiento.date()
+edad = fecha_actual - fecha_nacimiento
+print( "la edad de {} es: {}".format(fecha_nacimiento, edad))
 
