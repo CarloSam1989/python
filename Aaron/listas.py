7dd2de9b6521e6ff0ebc7378820bffdd0cfec5e0
AaronBlacio <aaronblacio_2003@outlook.com>
2023-06-05 19:56:44 -0500

    listas

--- Aaron/listas.py
+++ Aaron/listas.py
@@ -1,22 +1,25 @@
 listas = ["Sarango", "Aaron", "Landin", "García", "Tinoco"]
 print(listas)
-listas.insert(2, "changololuisa") #inserta en una possicon determinada
+listas.insert(2, "changololuisa")  # inserta en una possicon determinada
 print(listas)
-listas.append("siguenza") #inserta al final
+listas.append("siguenza")  # inserta al final
 print(listas)
-listas.remove("Aaron") #elimina por texto
+listas.remove("Aaron")  # elimina por texto
 print(listas)
-listas.pop(0) #elimina por index
+listas.pop(0)  # elimina por index
 print(listas)
-del listas[1] #elimina por index
+del listas[1]  # elimina por index
 print(listas)
-listas.clear() #elimina toda la lista
+listas.clear()  # elimina toda la lista
 print(listas)
 listas = ["Sarango", "Aaron", "Landin", "García", "Tinoco"]
-listas.sort() #Ordena de mayor a menor
+listas.sort()  # Ordena de mayor a menor
 print(listas)
-listas.sort(reverse=True) #Ordena de menor a mayor
+listas.sort(reverse=True)  # Ordena de menor a mayor
 print(listas)
-arreglo = sorted(listas) #Ordena de mayor a menor en otro arreglo
+arreglo = sorted(listas)  # Ordena de mayor a menor en otro arreglo
 print(listas)
+print(arreglo)
+listass = [["Siguenza", 0], ["Bonilla", 0], ["Landin", 10], ["Tinoco", 10]]
+arreglo = list(filter(lambda lista: lista[1] > 7, listass))
 print(arreglo)
